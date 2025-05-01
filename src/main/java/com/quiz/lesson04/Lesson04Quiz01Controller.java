e0fdc0a182c90f1cd72ecada3bcef70350c4f848
elroy0612 <elroy0612@gmail.com>
2025-05-01 13:14:00 +0900

    2024-05-01

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -2,11 +2,13 @@ package com.quiz.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.quiz.lesson04.domain.Seller;
 import com.quiz.lesson04.service.SellerBO;
 
 @RequestMapping("/lesson04/quiz01")
@@ -30,5 +32,21 @@ public class Lesson04Quiz01Controller {
 		sellerBO.addSeller(nickname, profileImageUrl, temperature);
 		
 		return "lesson04/afterAddSeller";
+	}
+	
+	@GetMapping("/seller-info-view")
+	public String sellerInfo(Model model,
+			@RequestParam(value = "id", required = false) Integer id) {
+		Seller seller;
+		if (id == null) {
+			seller = sellerBO.getLatestSeller();
+		} else {
+			seller = sellerBO.getSellerById(id);
+		}
+		
+		model.addAttribute("seller", seller);
+		model.addAttribute("title", "판매자 정보");
+		
+		return "lesson04/sellerInfo";
 	}
 }
--- src/main/java/com/quiz/lesson04/service/SellerBO.java
+++ src/main/java/com/quiz/lesson04/service/SellerBO.java
@@ -3,6 +3,7 @@ package com.quiz.lesson04.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.quiz.lesson04.domain.Seller;
 import com.quiz.lesson04.mapper.SellerMapper;
 
 @Service
@@ -13,5 +14,13 @@ public class SellerBO {
 	
 	public void addSeller(String nickname, String profileImageUrl, double temperature) {
 		sellerMapper.insertSeller(nickname, profileImageUrl, temperature);
+	}
+	
+	public Seller getLatestSeller() {
+		return sellerMapper.selectLatestSeller();
+	}
+	
+	public Seller getSellerById(int id) {
+		return sellerMapper.selectSellerById(id);
 	}
 }
